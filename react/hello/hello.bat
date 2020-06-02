5d6aa6af5d8794bfb691ba2e95df9014c8eb1241
hirtej <57080171+hirtej@users.noreply.github.com>
2020-06-03 01:22:09 +0900

    add hello/

---
+++ react/hello/hello.bat
@@ -0,0 +1,13 @@
+mkdir hello
+cd hello
+
+npm init -y
+
+rem EDIT package.json
+
+npm install 
+rem npm install react react-dom
+rem npm install webpack webpack-dev-server --save-dev
+rem npm install babel-cli babel-loader babel-preset-env babel-preset-react --save-dev
+rem npm install eslint eslint-loader eslint-plugin-react --save-dev
+rem npm install css-loader style-loader babel-loader --save-dev
