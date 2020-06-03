80ad5354d628e9916d481fa2b35348baf086fa9f
hirtej <57080171+hirtej@users.noreply.github.com>
2020-06-03 21:11:10 +0900

    hello_react

---
+++ react/hello_react/hello_react.bat
@@ -0,0 +1,14 @@
+npm init -y
+npm install react react-dom
+npm install webpack webpack-dev-server --save-dev
+npm install babel-cli babel-loader babel-preset-env babel-preset-react --save-dev
+npm install eslint eslint-loader eslint-plugin-react --save-dev
+npm install css-loader style-loader --save-dev
+
+npm install @material-ui/core
+
+npm install webpack-cli --save-dev
+npm install babel --save-dev
+npm install babel-loader@7 --save-dev
+
+npm start
